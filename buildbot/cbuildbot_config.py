11d9f92ed8c06b520edf2a94dd42920780a5ea37
David James <davidjames@chromium.org>
2011-09-01 15:51:20 -0700

    Enable uprevving on preflight bot for this branch.
    BUG=chrome-os-partner:5577
    TEST=Run cbuildbot_config_unittest.py
    
    Change-Id: I7a4fdaa515e4f263c14e38c98805e3d54b8ba502
    Reviewed-on: http://gerrit.chromium.org/gerrit/7113
    Reviewed-by: Dave Parker <dparker@chromium.org>
    Tested-by: David James <davidjames@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -495,7 +495,7 @@ add_config('tegra2_aebl-pre-flight-branch', [internal, {
 
   'uprev' : True,
   'overlays': 'both',
-  'push_overlays': None,
+  'push_overlays': 'both',
   'build_tests' : False,
   'unittests' : False,
   'vm_tests' : False,
