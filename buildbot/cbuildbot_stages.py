03b7c7561289e08b15c271d477c45132e273f9f6
Chris Sosa <sosa@chromium.org>
2012-06-29 11:42:37 -0700

    Prevent HWTest stage from killing a build.
    Max length without output is 9000, let's switch to something well
    below that to keep this from borking the build when the infrastructure
    hangs for too long.
    
    BUG=None
    TEST=Unittest
    
    Change-Id: Ife2cdccd2ef78f2e17719dbe39d745de89280074
    Reviewed-on: https://gerrit.chromium.org/gerrit/28141
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Tested-by: Chris Sosa <sosa@chromium.org>

--- buildbot/cbuildbot_stages.py
+++ buildbot/cbuildbot_stages.py
@@ -996,8 +996,8 @@ class VMTestStage(BoardSpecificBuilderStage):
 class HWTestStage(BoardSpecificBuilderStage, NonHaltingBuilderStage):
   """Stage that runs tests in the Autotest lab."""
 
-  # If the tests take longer than an hour and a half, abort.
-  INFRASTRUCTURE_TIMEOUT = 5400
+  # If the tests take longer than 2 hours and 20 min, abort.
+  INFRASTRUCTURE_TIMEOUT = 8400
   option_name = 'tests'
   config_name = 'hw_tests'
 
