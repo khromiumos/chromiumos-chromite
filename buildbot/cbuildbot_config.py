3902220ce4eb93ab1684bc24ee8589b67c772b6f
Peter Mayo <petermayo@chromium.org>
2013-03-19 01:07:29 -0400

    Remove extra PFQ on branch.
    BUG=chrome-os-partner:18182
    TEST=unittest (config)
    
    Change-Id: I67d65b2f3e7be642ff41e4569d48513f0c3db7ea
    Reviewed-on: https://gerrit.chromium.org/gerrit/45834
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Peter Mayo <petermayo@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -940,12 +940,6 @@ internal_incremental = internal.derive(
   description='Incremental Builds (internal)',
 )
 
-internal_pfq_branch.add_config('x86-alex-pre-flight-branch',
-  master=True,
-  push_overlays=constants.BOTH_OVERLAYS,
-  boards=['x86-alex'],
-)
-
 internal_pfq_branch.add_config('spring-pre-flight-branch',
   arm,
   master=True,
