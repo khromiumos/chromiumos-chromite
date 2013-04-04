9d47031cd45d5d25e145d1ce1e9796d7be295607
Mike Stipicevic <xusydoc@chromium.org>
2013-04-03 17:26:24 -0700

    CHERRY-PICK: Add stout32 builders.
    BUG=chromium:225458
    TEST=cbuildbot_config_unittest.py (minus already failing test due to
    "boards ['x86-generic'] are not watched on Chromium")
    
    Change-Id: Ib40c58600b93d6bb59d27479b597079ca96aae7b
    Original-Change-Id: I162b52fdb3fa099284402265211423e6d0286850
    Reviewed-on: https://gerrit.chromium.org/gerrit/47296
    Commit-Queue: Mike Stipicevic <xusydoc@chromium.org>
    Reviewed-by: Mike Stipicevic <xusydoc@chromium.org>
    Tested-by: Mike Stipicevic <xusydoc@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/47417
    Reviewed-by: David James <davidjames@chromium.org>
    Commit-Queue: Shawn Nematbakhsh <shawnn@chromium.org>
    Tested-by: Shawn Nematbakhsh <shawnn@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -970,6 +970,12 @@ internal_paladin.add_config('stout-paladin',
   paladin_builder_name='stout paladin',
 )
 
+internal_paladin.add_config('stout32-paladin',
+  boards=['stout32'],
+  paladin_builder_name='stout32 paladin',
+  important=False,
+)
+
 internal_paladin.add_config('butterfly-paladin',
   boards=['butterfly'],
   paladin_builder_name='butterfly paladin',
@@ -1082,6 +1088,10 @@ _release.add_config('stout-release',
   hw_tests_num=3,
 )
 
+_release.add_config('stout32-release',
+  boards=['stout32'],
+)
+
 _release.add_config('butterfly-release',
   boards=['butterfly'],
   useflags=official['useflags'] + ['oem_wallpaper'],
@@ -1144,6 +1154,7 @@ _x86_firmware_boards = (
   'lumpy',
   'parrot',
   'stout',
+  'stout32',
   'stumpy',
   'x86-mario',
 )
