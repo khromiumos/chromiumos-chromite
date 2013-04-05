f8eec67aadf01a33753c9c4c1e552dedeeb57abf
David James <davidjames@chromium.org>
2013-04-05 09:36:55 -0700

    Remove broken unittest.
    BUG=chromium:22693
    TEST=Run the unit test.
    
    Change-Id: I1398bc65316fb0481dee890e2261dd3a238a43f8
    (cherry picked from commit 3c1ea5bae54e99122a79f9867d6238c8407d9e85)
    Reviewed-on: https://gerrit.chromium.org/gerrit/47458
    Reviewed-by: David James <davidjames@chromium.org>
    Tested-by: David James <davidjames@chromium.org>

--- buildbot/cbuildbot_config_unittest.py
+++ buildbot/cbuildbot_config_unittest.py
@@ -8,21 +8,14 @@
 
 import json
 import os
-import re
 import subprocess
 import sys
-import urllib
 
 import constants
 sys.path.insert(0, constants.SOURCE_ROOT)
 from chromite.buildbot import cbuildbot_config
 from chromite.lib import cros_test_lib
 from chromite.lib import git
-
-CHROMIUM_WATCHING_URL = ("http://src.chromium.org/viewvc/" +
-    "chrome/trunk/tools/build/masters/" +
-    "master.chromium.chromiumos" + "/" +
-    "master_chromiumos_cros_cfg.py")
 
 # pylint: disable=W0212,R0904
 class CBuildBotTest(cros_test_lib.MoxTestCase):
@@ -173,31 +166,6 @@ class CBuildBotTest(cros_test_lib.MoxTestCase):
                         "ARM builder %s can't run vm tests!" % build_name)
         self.assertTrue(config['chrome_tests'] is False,
                         "ARM builder %s can't run chrome tests!" % build_name)
-
-  def testImportantMattersToChrome(self):
-    # TODO(ferringb): Decorate this as a network test.
-    namefinder = re.compile(r" *params='([^' ]*)[ ']")
-    req = urllib.urlopen(CHROMIUM_WATCHING_URL)
-    watched_configs = []
-    for m in [namefinder.match(line) for line in req.read().splitlines()]:
-      if m:
-        watched_configs.append(m.group(1))
-
-    watched_boards = []
-    for config in watched_configs:
-      watched_boards.extend(cbuildbot_config.config[config]['boards'])
-
-    watched_boards = set(watched_boards)
-
-    for build_name, config in cbuildbot_config.config.iteritems():
-      if (config['important'] and
-          config['chrome_rev'] == constants.CHROME_REV_LATEST and
-          config['overlays'] == constants.PUBLIC_OVERLAYS and
-          config['build_type'] == constants.CHROME_PFQ_TYPE):
-        boards = set(config['boards'])
-        self.assertTrue(boards.issubset(watched_boards),
-                        'Config %s: boards %r are not watched on Chromium' %
-                        (build_name, list(boards - watched_boards)))
 
   #TODO: Add test for compare functionality
   def testJSONDumpLoadable(self):
