da46fae44421533d0c6b92666d9841fde5a9090f
Mike Stipicevic <stip@chromium.org>
2013-11-21 15:49:57 -0800

    Add monroe pre-flight config.
    TEST=buildbot/cbuildbot_config_unittest.py
    BUG=chromium:312812
    
    Change-Id: I9aef07b397b29c25d44787ae059b8c3f15cd22a2
    Reviewed-on: https://chromium-review.googlesource.com/177651
    Tested-by: Mike Stipicevic <stip@chromium.org>
    Reviewed-by: Peter Mayo <petermayo@chromium.org>
    Tested-by: Peter Mayo <petermayo@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -1205,10 +1205,10 @@ sonic = _config(
   hw_tests=[],
 )
 
-internal_pfq_branch.add_config('x86-alex-pre-flight-branch',
+internal_pfq_branch.add_config('monroe-pre-flight-branch',
   master=True,
   push_overlays=constants.BOTH_OVERLAYS,
-  boards=['x86-alex'],
+  boards=['monroe'],
 )
 
 internal.add_config('test-ap',
