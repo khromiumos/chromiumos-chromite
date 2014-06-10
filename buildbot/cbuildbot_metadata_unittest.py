bloomberg/chromium.bb

Write per-builder pass/fail status to git repos.
BUG=chromium:376008
TEST=New unit tests.

Change-Id: I96d45392b612f5c27b9ad5276202afbd0c5e907c
Reviewed-on: https://chromium-review.googlesource.com/202222
Reviewed-by: Aviv Keshet <akeshet@chromium.org>
Commit-Queue: David James <davidjames@chromium.org>
Tested-by: David James <davidjames@chromium.org>

cbuildbot/cbuildbot_metadata_unittest.py
@@ -12,8 +12,65 @@ import sys
 
 sys.path.insert(0, os.path.abspath('%s/../..' % os.path.dirname(__file__)))
 from chromite.cbuildbot import cbuildbot_metadata
+from chromite.cbuildbot import cbuildbot_results as results_lib
+from chromite.cbuildbot import constants
 from chromite.lib import cros_test_lib
 from chromite.lib import parallel
+
+
+class LocalBuilderStatusTest(cros_test_lib.TestCase):
+  """Test the correctness of the various LocalBuilderStatus methods."""
+
+  Results = results_lib.Results
+  SUCCESS = results_lib.Results.SUCCESS
+
+  def setUp(self):
+    self.Results.Clear()
+
+  def tearDown(self):
+    self.Results.Clear()
+
+  def testEmptyFail(self):
+    """Sometimes there is no board-specific information."""
+    self.Results.Record('Sync', self.SUCCESS)
+    builder_status = cbuildbot_metadata.LocalBuilderStatus.Get()
+    self.assertEqual({}, builder_status.board_status_map)
+    self.assertEqual(constants.FINAL_STATUS_FAILED,
+                     builder_status.GetBuilderStatus('lumpy-paladin'))
+    self.assertEqual(constants.FINAL_STATUS_PASSED,
+                     builder_status.GetBuilderStatus('master-paladin'))
+
+  def testAllFail(self):
+    """Failures in a shared stage cause specific stages to fail."""
+    self.Results.Record('Sync', self.SUCCESS)
+    self.Results.Record('BuildPackages', self.SUCCESS, board='lumpy')
+    self.Results.Record('Completion', 'FAIL')
+    builder_status = cbuildbot_metadata.LocalBuilderStatus.Get()
+    self.assertEqual({'lumpy': constants.FINAL_STATUS_PASSED},
+                     builder_status.board_status_map)
+    self.assertEqual(constants.FINAL_STATUS_FAILED,
+                     builder_status.GetBuilderStatus('lumpy-paladin'))
+    self.assertEqual(constants.FINAL_STATUS_FAILED,
+                     builder_status.GetBuilderStatus('master-paladin'))
+
+  def testMixedFail(self):
+    """Specific stages can pass/fail separately."""
+    self.Results.Record('Sync', self.SUCCESS)
+    self.Results.Record('BuildPackages [lumpy]', 'FAIL', board='lumpy')
+    self.Results.Record('BuildPackages [stumpy]', self.SUCCESS, board='stumpy')
+    builder_status = cbuildbot_metadata.LocalBuilderStatus.Get()
+    self.assertEqual({'lumpy': constants.FINAL_STATUS_FAILED,
+                      'stumpy': constants.FINAL_STATUS_PASSED},
+                     builder_status.board_status_map)
+    self.assertEqual(constants.FINAL_STATUS_FAILED,
+                     builder_status.GetBuilderStatus('lumpy-paladin'))
+    self.assertEqual(constants.FINAL_STATUS_PASSED,
+                     builder_status.GetBuilderStatus('stumpy-paladin'))
+    self.assertEqual(constants.FINAL_STATUS_FAILED,
+                     builder_status.GetBuilderStatus('winky-paladin'))
+    self.assertEqual(constants.FINAL_STATUS_PASSED,
+                     builder_status.GetBuilderStatus('master-paladin'))
+
 
 @cros_test_lib.NetworkTest()
 class MetadataFetchTest(cros_test_lib.TestCase):
@@ -27,13 +84,14 @@ class MetadataFetchTest(cros_test_lib.TestCase):
     metadata_dict = metadata._metadata_dict # pylint: disable=W0212
     self.assertEqual(metadata_dict['status']['status'], 'passed')
 
+
 class MetadataTest(cros_test_lib.TestCase):
   """Tests the correctness of various metadata methods."""
 
   def testGetDict(self):
-    starting_dict = {'key1' : 1,
-                     'key2' : '2',
-                     'cl_actions' : [('a', 1), ('b', 2)]}
+    starting_dict = {'key1': 1,
+                     'key2': '2',
+                     'cl_actions': [('a', 1), ('b', 2)]}
     metadata = cbuildbot_metadata.CBuildbotMetadata(starting_dict)
     ending_dict = metadata.GetDict()
     self.assertEqual(starting_dict, ending_dict)
@@ -42,10 +100,10 @@ class MetadataTest(cros_test_lib.TestCase):
     m = multiprocessing.Manager()
     metadata = cbuildbot_metadata.CBuildbotMetadata(multiprocess_manager=m)
     key_dict = {'key1': 1, 'key2': 2}
-    starting_dict = {'key1' : 1,
-                     'key2' : '2',
-                     'key3' : key_dict,
-                     'cl_actions' : [('a', 1), ('b', 2)]}
+    starting_dict = {'key1': 1,
+                     'key2': '2',
+                     'key3': key_dict,
+                     'cl_actions': [('a', 1), ('b', 2)]}
 
     # Test that UpdateWithDict is process-safe
     parallel.RunParallelSteps([lambda: metadata.UpdateWithDict(starting_dict)])
@@ -67,6 +125,7 @@ class MetadataTest(cros_test_lib.TestCase):
     # Assert that an action was recorded.
     self.assertEqual(len(starting_dict['cl_actions']) + 1,
                      len(ending_dict['cl_actions']))
+
 
 if __name__ == '__main__':
   cros_test_lib.main(level=logging.DEBUG)

cbuildbot/manifest_version.py
@@ -14,7 +14,7 @@ import re
 import shutil
 import tempfile
 
-from chromite.cbuildbot import constants, repository
+from chromite.cbuildbot import cbuildbot_metadata, constants, repository
 from chromite.lib import cros_build_lib
 from chromite.lib import git
 from chromite.lib import gs
@@ -756,21 +756,18 @@ class BuildSpecsManager(object):
         raise GenerateBuildSpecException('Builder already inflight')
       raise GenerateBuildSpecException(e)
 
-  def _SetFailed(self):
-    """Marks the buildspec as failed by creating a symlink in fail dir."""
-    filename = '%s.xml' % self.current_version
-    src_file = os.path.join(self.all_specs_dir, filename)
-    for fail_dir in self.fail_dirs:
-      dest_file = os.path.join(fail_dir, filename)
-      logging.debug('Setting build to failed  %s: %s', src_file, dest_file)
-      CreateSymlink(src_file, dest_file)
-
-  def _SetPassed(self):
+  def _SetPassSymlinks(self):
     """Marks the buildspec as passed by creating a symlink in passed dir."""
+    local_builder_status = cbuildbot_metadata.LocalBuilderStatus.Get()
     src_file = '%s.xml' % os.path.join(self.all_specs_dir, self.current_version)
-    for pass_dir in self.pass_dirs:
+    for i, build_name in enumerate(self.build_names):
+      status = local_builder_status.GetBuilderStatus(build_name)
+      if status == constants.FINAL_STATUS_PASSED:
+        pass_dir = self.pass_dirs[i]
+      else:
+        pass_dir = self.fail_dirs[i]
       dest_file = '%s.xml' % os.path.join(pass_dir, self.current_version)
-      logging.debug('Setting build to passed  %s: %s', src_file, dest_file)
+      logging.debug('Build %s: %s -> %s', status, src_file, dest_file)
       CreateSymlink(src_file, dest_file)
 
   def PushSpecChanges(self, commit_message):
@@ -798,10 +795,8 @@ class BuildSpecsManager(object):
                           '%s' % (BuilderStatus.GetCompletedStatus(success),
                                   self.current_version,
                                   self.build_names[0]))
-        if success:
-          self._SetPassed()
-        else:
-          self._SetFailed()
+
+        self._SetPassSymlinks()
 
         self.PushSpecChanges(commit_message)
       except cros_build_lib.RunCommandError as e:

khromiumos/chromiumos-chromite

Change-Id: Ic93c29845174f67f40a0c0d288cc194593bff6d9
Original-Change-Id: I96d45392b612f5c27b9ad5276202afbd0c5e907c
Originally-Reviewed-on: https://chromium-review.googlesource.com/202222
Reviewed-on: https://chromium-review.googlesource.com/203100
Reviewed-by: Don Garrett <dgarrett@chromium.org>

buildbot/cbuildbot_metadata_unittest.py
@@ -12,8 +12,65 @@ import sys
 
 sys.path.insert(0, os.path.abspath('%s/../..' % os.path.dirname(__file__)))
 from chromite.buildbot import cbuildbot_metadata
+from chromite.buildbot import cbuildbot_results as results_lib
+from chromite.buildbot import constants
 from chromite.lib import cros_test_lib
 from chromite.lib import parallel
+
+
+class LocalBuilderStatusTest(cros_test_lib.TestCase):
+  """Test the correctness of the various LocalBuilderStatus methods."""
+
+  Results = results_lib.Results
+  SUCCESS = results_lib.Results.SUCCESS
+
+  def setUp(self):
+    self.Results.Clear()
+
+  def tearDown(self):
+    self.Results.Clear()
+
+  def testEmptyFail(self):
+    """Sometimes there is no board-specific information."""
+    self.Results.Record('Sync', self.SUCCESS)
+    builder_status = cbuildbot_metadata.LocalBuilderStatus.Get()
+    self.assertEqual({}, builder_status.board_status_map)
+    self.assertEqual(constants.FINAL_STATUS_FAILED,
+                     builder_status.GetBuilderStatus('lumpy-paladin'))
+    self.assertEqual(constants.FINAL_STATUS_PASSED,
+                     builder_status.GetBuilderStatus('master-paladin'))
+
+  def testAllFail(self):
+    """Failures in a shared stage cause specific stages to fail."""
+    self.Results.Record('Sync', self.SUCCESS)
+    self.Results.Record('BuildPackages', self.SUCCESS, board='lumpy')
+    self.Results.Record('Completion', 'FAIL')
+    builder_status = cbuildbot_metadata.LocalBuilderStatus.Get()
+    self.assertEqual({'lumpy': constants.FINAL_STATUS_PASSED},
+                     builder_status.board_status_map)
+    self.assertEqual(constants.FINAL_STATUS_FAILED,
+                     builder_status.GetBuilderStatus('lumpy-paladin'))
+    self.assertEqual(constants.FINAL_STATUS_FAILED,
+                     builder_status.GetBuilderStatus('master-paladin'))
+
+  def testMixedFail(self):
+    """Specific stages can pass/fail separately."""
+    self.Results.Record('Sync', self.SUCCESS)
+    self.Results.Record('BuildPackages [lumpy]', 'FAIL', board='lumpy')
+    self.Results.Record('BuildPackages [stumpy]', self.SUCCESS, board='stumpy')
+    builder_status = cbuildbot_metadata.LocalBuilderStatus.Get()
+    self.assertEqual({'lumpy': constants.FINAL_STATUS_FAILED,
+                      'stumpy': constants.FINAL_STATUS_PASSED},
+                     builder_status.board_status_map)
+    self.assertEqual(constants.FINAL_STATUS_FAILED,
+                     builder_status.GetBuilderStatus('lumpy-paladin'))
+    self.assertEqual(constants.FINAL_STATUS_PASSED,
+                     builder_status.GetBuilderStatus('stumpy-paladin'))
+    self.assertEqual(constants.FINAL_STATUS_FAILED,
+                     builder_status.GetBuilderStatus('winky-paladin'))
+    self.assertEqual(constants.FINAL_STATUS_PASSED,
+                     builder_status.GetBuilderStatus('master-paladin'))
+
 
 @cros_test_lib.NetworkTest()
 class MetadataFetchTest(cros_test_lib.TestCase):
@@ -27,13 +84,14 @@ class MetadataFetchTest(cros_test_lib.TestCase):
     metadata_dict = metadata._metadata_dict # pylint: disable=W0212
     self.assertEqual(metadata_dict['status']['status'], 'passed')
 
+
 class MetadataTest(cros_test_lib.TestCase):
   """Tests the correctness of various metadata methods."""
 
   def testGetDict(self):
-    starting_dict = {'key1' : 1,
-                     'key2' : '2',
-                     'cl_actions' : [('a', 1), ('b', 2)]}
+    starting_dict = {'key1': 1,
+                     'key2': '2',
+                     'cl_actions': [('a', 1), ('b', 2)]}
     metadata = cbuildbot_metadata.CBuildbotMetadata(starting_dict)
     ending_dict = metadata.GetDict()
     self.assertEqual(starting_dict, ending_dict)
@@ -41,9 +99,11 @@ class MetadataTest(cros_test_lib.TestCase):
   def testMultiprocessSafety(self):
     m = multiprocessing.Manager()
     metadata = cbuildbot_metadata.CBuildbotMetadata(multiprocess_manager=m)
-    starting_dict = {'key1' : 1,
-                     'key2' : '2',
-                     'cl_actions' : [('a', 1), ('b', 2)]}
+    key_dict = {'key1': 1, 'key2': 2}
+    starting_dict = {'key1': 1,
+                     'key2': '2',
+                     'key3': key_dict,
+                     'cl_actions': [('a', 1), ('b', 2)]}
 
     # Test that UpdateWithDict is process-safe
     parallel.RunParallelSteps([lambda: metadata.UpdateWithDict(starting_dict)])
@@ -59,6 +119,7 @@ class MetadataTest(cros_test_lib.TestCase):
     # Assert that an action was recorded.
     self.assertEqual(len(starting_dict['cl_actions']) + 1,
                      len(ending_dict['cl_actions']))
+
 
 if __name__ == '__main__':
   cros_test_lib.main(level=logging.DEBUG)

buildbot/manifest_version.py
@@ -14,7 +14,7 @@ import re
 import shutil
 import tempfile
 
-from chromite.buildbot import constants, repository
+from chromite.buildbot import cbuildbot_metadata, constants, repository
 from chromite.lib import cros_build_lib
 from chromite.lib import git
 from chromite.lib import gs
@@ -763,21 +763,18 @@ class BuildSpecsManager(object):
         raise GenerateBuildSpecException('Builder already inflight')
       raise GenerateBuildSpecException(e)
 
-  def _SetFailed(self):
-    """Marks the buildspec as failed by creating a symlink in fail dir."""
-    filename = '%s.xml' % self.current_version
-    src_file = os.path.join(self.all_specs_dir, filename)
-    for fail_dir in self.fail_dirs:
-      dest_file = os.path.join(fail_dir, filename)
-      logging.debug('Setting build to failed  %s: %s', src_file, dest_file)
-      CreateSymlink(src_file, dest_file)
-
-  def _SetPassed(self):
+  def _SetPassSymlinks(self):
     """Marks the buildspec as passed by creating a symlink in passed dir."""
+    local_builder_status = cbuildbot_metadata.LocalBuilderStatus.Get()
     src_file = '%s.xml' % os.path.join(self.all_specs_dir, self.current_version)
-    for pass_dir in self.pass_dirs:
+    for i, build_name in enumerate(self.build_names):
+      status = local_builder_status.GetBuilderStatus(build_name)
+      if status == constants.FINAL_STATUS_PASSED:
+        pass_dir = self.pass_dirs[i]
+      else:
+        pass_dir = self.fail_dirs[i]
       dest_file = '%s.xml' % os.path.join(pass_dir, self.current_version)
-      logging.debug('Setting build to passed  %s: %s', src_file, dest_file)
+      logging.debug('Build %s: %s -> %s', status, src_file, dest_file)
       CreateSymlink(src_file, dest_file)
 
   def PushSpecChanges(self, commit_message):
@@ -805,10 +802,8 @@ class BuildSpecsManager(object):
                           '%s' % (BuilderStatus.GetCompletedStatus(success),
                                   self.current_version,
                                   self.build_names[0]))
-        if success:
-          self._SetPassed()
-        else:
-          self._SetFailed()
+
+        self._SetPassSymlinks()
 
         self.PushSpecChanges(commit_message)
       except cros_build_lib.RunCommandError as e: