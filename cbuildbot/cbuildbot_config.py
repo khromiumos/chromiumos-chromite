5811087a37939cd3ebd5c471cadbbd31103ac814
Bernie Thompson <bhthompson@chromium.org>
2014-10-06 13:51:54 -0700

    cbuildbot_config: migrate branch to storm pre-flight
    We can only have a storm pre-flight builder on this branch, so modify
    the configuration to make it so.
    
    BUG=chromium:418678
    TEST=None
    
    Change-Id: I84e91cf6f24407fd53e5060d57d41df13a5d2729
    Reviewed-on: https://chromium-review.googlesource.com/221770
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Queue: Bernie Thompson <bhthompson@chromium.org>
    Tested-by: Bernie Thompson <bhthompson@chromium.org>

--- cbuildbot/cbuildbot_config.py
+++ cbuildbot/cbuildbot_config.py
@@ -1739,15 +1739,6 @@ internal_incremental = internal.derive(
   description='Incremental Builds (internal)',
 )
 
-internal_pfq_branch.add_config('lumpy-pre-flight-branch',
-  master=True,
-  push_overlays=constants.BOTH_OVERLAYS,
-  boards=['lumpy'],
-  afdo_generate=True,
-  afdo_update_ebuild=True,
-  hw_tests=[AFDORecordTest()],
-)
-
 # A test-ap image is just a test image with a special profile enabled.
 # Note that each board enabled for test-ap use has to have the testbed-ap
 # profile linked to from its private overlay.
@@ -2788,6 +2779,12 @@ def _AddFirmwareConfigs():
 
 _AddFirmwareConfigs()
 
+internal_pfq_branch.add_config('storm-pre-flight-branch',
+  _firmware,
+  master=True,
+  push_overlays=constants.BOTH_OVERLAYS,
+  boards=['storm'],
+)
 
 # This is an example factory branch configuration for x86.
 # Modify it to match your factory branch.
