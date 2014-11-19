ef73b5b69a7424436be7cd8fc1c89ebf4cd6cb02
Shawn Nematbakhsh <shawnn@chromium.org>
2014-11-18 18:23:58 -0800

    Add candy pre-flight builder
    We only need a Candy preflight builder on this branch.
    
    BUG=chrome-os-partner:33789
    TEST=None
    
    Change-Id: I710b6aa598c5adcaf8b62c26a6a4d018afb399e6
    Reviewed-on: https://chromium-review.googlesource.com/230588
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>
    Commit-Queue: Shawn Nematbakhsh <shawnn@chromium.org>
    Tested-by: Shawn Nematbakhsh <shawnn@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -2109,11 +2109,11 @@ def _AddFirmwareConfigs():
 
 _AddFirmwareConfigs()
 
-internal_pfq_branch.add_config('rambi-pre-flight-branch',
+internal_pfq_branch.add_config('candy-pre-flight-branch',
   _firmware_pfq,
   master=True,
   push_overlays=constants.BOTH_OVERLAYS,
-  boards=['rambi'],
+  boards=['candy'],
 )
 
 # This is an example factory branch configuration for x86.
