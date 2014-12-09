45f459eaf5ffcfb34469824e9e751bd1491175df
Mike Frysinger <vapier@chromium.org>
2014-12-09 15:51:52 -0500

    paygen: make keyset filter a bit more flexible
    Support using the full keyset name in the signed image names and not
    just the stripped down one.  This lets us sign the same image with
    keysets from different boards and host them in the same dir.  Otherwise
    the filenames are not unique.
    
    BUG=chrome-os-partner:34150
    TEST=`./paygen_build_lib.py` passes
    
    Change-Id: I1fecd262df8b4385884cb72a0cf51bf4ca3775fb
    Previous-Reviewed-on: https://chromium-review.googlesource.com/234200
    Trybot-Ready: Mike Frysinger <vapier@chromium.org>
    (cherry picked from commit 75cb59c37287862cca753f6e118b61ff5123b2a5)
    Reviewed-on: https://chromium-review.googlesource.com/234383
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- lib/paygen/paygen_build_lib.py
+++ lib/paygen/paygen_build_lib.py
@@ -168,7 +168,7 @@ def _FilterForMp(artifacts):
   Returns:
     List of MP images.
   """
-  return [i for i in _FilterForImages(artifacts) if i.key.startswith('mp')]
+  return [i for i in _FilterForImages(artifacts) if 'mp' in i.key.split('-')]
 
 
 def _FilterForPremp(artifacts):
@@ -183,7 +183,7 @@ def _FilterForPremp(artifacts):
   Returns:
     List of PreMP images.
   """
-  return [i for i in _FilterForImages(artifacts) if i.key == 'premp']
+  return [i for i in _FilterForImages(artifacts) if 'premp' in i.key.split('-')]
 
 
 def _FilterForBasic(artifacts):
