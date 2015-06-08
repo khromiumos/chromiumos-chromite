e9604a7d040d4f4be72881f8ffd27d0e54499156
Bernie Thompson <bhthompson@chromium.org>
2015-06-08 15:04:30 -0700

    Enable Orco preflight builder config
    BUG=chrome-os-partner:41010
    TEST=None
    
    Change-Id: Ibde9a2d35556aa9c66f856959d91b84d895d3646
    Reviewed-on: https://chromium-review.googlesource.com/276240
    Commit-Queue: Bernie Thompson <bhthompson@chromium.org>
    Tested-by: Bernie Thompson <bhthompson@chromium.org>
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -2117,11 +2117,11 @@ def _AddFirmwareConfigs():
 
 _AddFirmwareConfigs()
 
-internal_pfq_branch.add_config('rambi-pre-flight-branch',
+internal_pfq_branch.add_config('orco-pre-flight-branch',
   _firmware_pfq,
   master=True,
   push_overlays=constants.BOTH_OVERLAYS,
-  boards=['rambi'],
+  boards=['orco'],
 )
 
 # This is an example factory branch configuration for x86.
