1c2f7986848082c68b479b0cc790ed3781e7c0a0
Bernie Thompson <bhthompson@chromium.org>
2015-07-15 18:02:11 -0700

    Fix up factory branch configuration for smaug
    BUG=chrome-os-partner:42697
    TEST=None
    
    Change-Id: I4b621aa71edbb8e8e1baf355384f54a3f6923169
    Reviewed-on: https://chromium-review.googlesource.com/286006
    Commit-Queue: Bernie Thompson <bhthompson@chromium.org>
    Tested-by: Bernie Thompson <bhthompson@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: David Riley <davidriley@chromium.org>

--- cbuildbot/chromeos_config.py
+++ cbuildbot/chromeos_config.py
@@ -1420,13 +1420,11 @@ def GetConfig():
   )
 
   site_config.AddConfig(
-      internal_pfq_branch, 'lumpy-pre-flight-branch',
+      internal_pfq_branch, 'smaug-pre-flight-branch',
       master=True,
+      vm_tests=None,
       push_overlays=constants.BOTH_OVERLAYS,
-      boards=['lumpy'],
-      afdo_generate=True,
-      afdo_update_ebuild=True,
-      hw_tests=[HWTestList.AFDORecordTest()],
+      boards=['smaug'],
   )
 
   # A test-ap image is just a test image with a special profile enabled.
@@ -2696,8 +2694,9 @@ def GetConfig():
   # This is an example factory branch configuration.
   # Modify it to match your factory branch.
   site_config.AddConfig(
-      _factory_release, 'x86-mario-factory',
-      boards=['x86-mario'],
+      _factory_release, 'smaug-factory',
+      vm_tests=None,
+      boards=['smaug'],
   )
 
   _payloads = site_config.AddTemplate(
