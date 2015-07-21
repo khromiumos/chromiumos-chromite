a0bfc3016ab8388bc30293862c7218dca75cb8d6
Bernie Thompson <bhthompson@chromium.org>
2015-07-21 09:02:38 -0700

    Add strago firmware pre-flight builder config
    BUG=chrome-os-partner:42759
    TEST=None
    
    Change-Id: I5ee668fc3bf35fa3dd25d0ecc523bdad5161fb59
    Reviewed-on: https://chromium-review.googlesource.com/287265
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Commit-Queue: Bernie Thompson <bhthompson@chromium.org>
    Tested-by: Bernie Thompson <bhthompson@chromium.org>

--- cbuildbot/chromeos_config.py
+++ cbuildbot/chromeos_config.py
@@ -1414,16 +1414,6 @@ def GetConfig():
       description='Incremental Builds (internal)',
   )
 
-  site_config.AddConfig(
-      internal_pfq_branch, 'lumpy-pre-flight-branch',
-      master=True,
-      push_overlays=constants.BOTH_OVERLAYS,
-      boards=['lumpy'],
-      afdo_generate=True,
-      afdo_update_ebuild=True,
-      hw_tests=[HWTestList.AFDORecordTest()],
-  )
-
   # A test-ap image is just a test image with a special profile enabled.
   # Note that each board enabled for test-ap use has to have the testbed-ap
   # profile linked to from its private overlay.
@@ -2689,6 +2679,13 @@ def GetConfig():
 
   _AddFirmwareConfigs()
 
+  site_config.AddConfig(
+      internal_pfq_branch, 'strago-pre-flight-branch',
+      _firmware,
+      master=True,
+      push_overlays=constants.BOTH_OVERLAYS,
+      boards=['strago'],
+  )
 
   # This is an example factory branch configuration.
   # Modify it to match your factory branch.
