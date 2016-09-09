393e3b4962cf9c102e18780bfb95530fc51fd546
Mohammed Habibulla <moch@google.com>
2016-09-09 15:18:47 -0700

    Kevin: add config for pre-flight-branch
    BUG=none
    TEST=none
    
    Change-Id: I04bc958ad9b3d4d759fe1580812b5702c30eb5d4
    Reviewed-on: https://chromium-review.googlesource.com/383834
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>
    Commit-Queue: Mohammed Habibulla <moch@google.com>
    Tested-by: Mohammed Habibulla <moch@google.com>

--- cbuildbot/chromeos_config.py
+++ cbuildbot/chromeos_config.py
@@ -1641,15 +1641,15 @@ def GetConfig():
   )
 
   site_config.Add(
-      'samus-pre-flight-branch', internal_pfq_branch,
+      'kevin-pre-flight-branch', internal_pfq_branch,
       master=True,
       push_overlays=constants.BOTH_OVERLAYS,
-      boards=['samus'],
+      boards=['kevin'],
       android_rev=constants.ANDROID_REV_LATEST,
       afdo_generate=True,
       afdo_update_ebuild=True,
       vm_tests=[],
-      hw_tests=[HWTestList.AFDORecordTest()],
+      hw_tests=[],
   )
 
   # A test-ap image is just a test image with a special profile enabled.
