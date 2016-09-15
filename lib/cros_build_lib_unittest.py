e6f9782cb94e0b786a19537544d3707bd831fc16
Mike Frysinger <vapier@chromium.org>
2016-09-15 17:17:56 -0400

    cros_build_lib: disable repo manifest tests
    These rely on ToT repo code which can break things.  Disable tests in
    branches since it's unlikely this code will break/change.  We'll find
    a better solution in ToT.
    
    BUG=chromium:647414
    TEST=`./lib/cros_build_lib_unittest` no longer fails in branch w/ToT repo
    
    Change-Id: I8133bff80cc64bd6c5c7bf43df6f32c058eb7987
    Reviewed-on: https://chromium-review.googlesource.com/386202
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- lib/cros_build_lib_unittest.py
+++ lib/cros_build_lib_unittest.py
@@ -19,6 +19,7 @@ import socket
 import StringIO
 import sys
 import time
+import unittest
 import __builtin__
 
 from chromite.cbuildbot import constants
@@ -1256,6 +1257,7 @@ class TestManifestCheckout(cros_test_lib.TempDirTestCase):
     self.active_manifest = os.path.realpath(
         os.path.join(self.tempdir, '.repo', 'manifest.xml'))
 
+  @unittest.skip('Relies on ToT repo code https://crbug.com/647414')
   def testManifestInheritance(self):
     osutils.WriteFile(self.active_manifest, """
         <manifest>
@@ -1288,6 +1290,7 @@ class TestManifestCheckout(cros_test_lib.TempDirTestCase):
     self.assertEqual(list(manifest.remotes), ['foon'])
 
   # pylint: disable=E1101
+  @unittest.skip('Relies on ToT repo code https://crbug.com/647414')
   def testGetManifestsBranch(self):
     func = git.ManifestCheckout._GetManifestsBranch
     manifest = self.manifest_dir
@@ -1338,6 +1341,7 @@ class TestManifestCheckout(cros_test_lib.TempDirTestCase):
     git.RunGit(manifest, ['branch', '-d', 'default'])
     assertExcept("It should be checked out to 'default'")
 
+  @unittest.skip('Relies on ToT repo code https://crbug.com/647414')
   def testGitMatchBranchName(self):
     git_repo = os.path.join(self.tempdir, '.repo', 'manifests')
 
