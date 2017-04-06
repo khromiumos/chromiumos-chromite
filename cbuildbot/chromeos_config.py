f58e59955d033b937a0b70165858ba0df0adf81a
Don Garrett <dgarrett@google.com>
2017-04-06 16:26:13 -0700

    chromeos_config: Specify chrome_rev in pre-flight-branch.
    We have been specifying the chrome_rev option on the command line via
    a recipe configuration. Doing it in the build config like everything
    else is much easier to mainatin.
    
    After this is landed, and merged out to active branches, I'll remove
    the recipe configuration.
    
    BUG=chromium:709287
    TEST=Unittests.
    
    Change-Id: Ia2b0b6f735eadae988f6edb9aad866f8c4d340d5
    Reviewed-on: https://chromium-review.googlesource.com/470706
    Commit-Ready: Don Garrett <dgarrett@chromium.org>
    Tested-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>
    (cherry picked from commit ec71d62b63764cd5b483dfd9641cf38d0e27567e)
    Reviewed-on: https://chromium-review.googlesource.com/471807
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Commit-Queue: Don Garrett <dgarrett@chromium.org>

--- cbuildbot/chromeos_config.py
+++ cbuildbot/chromeos_config.py
@@ -3499,6 +3499,7 @@ def SpecialtyBuilders(site_config, boards_dict, ge_build_config):
       android_rev=constants.ANDROID_REV_LATEST,
       afdo_generate=True,
       afdo_update_ebuild=True,
+      chrome_rev=constants.CHROME_REV_STICKY,
       vm_tests=[],
       hw_tests=[hw_test_list.AFDORecordTest()],
       useflags=append_useflags(['-transparent_hugepage']),
