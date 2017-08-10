bloomberg/chromium.bb

paygen_build_lib: Add GetPaygenJson().
We are replacing all paygen config files with the new paygen.json.

This CL adds logic to fetch/parse the new file, and uses it to replace
boards.json.

BUG=chromium:735191
TEST=Unittests

Change-Id: I3e0d91295a9c18ea427c72772433792a7d7ab406
Reviewed-on: https://chromium-review.googlesource.com/557360
Commit-Ready: Don Garrett <dgarrett@chromium.org>
Tested-by: Don Garrett <dgarrett@chromium.org>
Reviewed-by: Don Garrett <dgarrett@chromium.org>

lib/paygen/paygen_build_lib.py
@@ -65,9 +65,9 @@ RUN_SUITE_MIN_MSTONE = 30
 PAYGEN_LOG_TIMESTAMP_FORMAT = '%Y%m%d-%H%M%S-UTC'
 
 # Board and device information published by goldeneye.
-BOARDS_URI = 'gs://chromeos-build-release-console/boards.json'
 FSI_URI = 'gs://chromeos-build-release-console/fsis.json'
 OMAHA_URI = 'gs://chromeos-build-release-console/omaha_status.json'
+PAYGEN_URI = 'gs://chromeos-build-release-console/paygen.json'
 
 # Max number of attempts to download and parse a JSON file.
 JSON_PARSE_RETRY_COUNT = 2
@@ -397,8 +397,8 @@ class _PaygenBuild(object):
                          _FindControlFileDir(self._work_dir))
 
     # Cached goldeneye data.
-    self.cachedFsisJson = {}
-    self.cachedOmahaJson = {}
+    self.cachedFsisJson = None
+    self.cachedOmahaJson = None
 
   def _GetFsisJson(self):
     if not self.cachedFsisJson:
@@ -409,6 +409,49 @@ class _PaygenBuild(object):
     if not self.cachedOmahaJson:
       self.cachedOmahaJson = _GetJson(OMAHA_URI)
     return self.cachedOmahaJson
+
+  # Hidden class level cache value.
+  _cachedPaygenJson = None
+
+  @classmethod
+  def GetPaygenJson(cls, board=None, channel=None):
+    """Fetch the parsed Golden Eye payload generation configuration.
+
+    Args:
+      board: Board name in builder format (not release) or None for '*'
+      channel: Channel name in 'stable' format or None for '*'
+
+    Returns:
+      List of GE delta values matching specification. Sample delta value:
+
+      {
+        'board': {
+          'public_codename': 'x86-alex',
+          'is_active': true,
+          'builder_name': 'x86-alex'
+        },
+        'delta_type': 'MILESTONE',
+        'channel': 'stable',
+        'chrome_os_version': '8530.81.0',
+        'chrome_version': '53.0.2785.103',
+        'milestone': 53,
+        'generate_delta': true,
+        'delta_payload_tests': true,
+        'full_payload_tests': false
+      }
+    """
+    if not cls._cachedPaygenJson:
+      cls._cachedPaygenJson = _GetJson(PAYGEN_URI)
+
+    result = []
+
+    for delta in cls._cachedPaygenJson['delta']:
+      if ((board and delta['board']['builder_name'] != board) or
+          (channel and delta['channel'] != channel)):
+        continue
+      result.append(delta)
+
+    return result
 
   def _GetFlagURI(self, flag):
     """Find the URI of the lock file associated with this build.
@@ -1403,12 +1446,8 @@ def ValidateBoardConfig(board):
   Raises:
     BoardNotConfigured if the board is unknown.
   """
-  # Right now, we just validate that the board exists.
-  boards = _GetJson(BOARDS_URI)
-  for b in boards.get('boards', []):
-    if b['public_codename'] == board:
-      return
-  raise BoardNotConfigured(board)
+  if not _PaygenBuild.GetPaygenJson(board):
+    raise BoardNotConfigured(board)
 
 
 def CreatePayloads(build, work_dir, site_config,

khromiumos/chromiumos-chromite

RELANDING:
  Rev 1 of this CL was previously landed as CL:557360.
  Diff Rev 1 and final Rev to see the fix.
Change-Id: I95d88a36bea70dfce61b95ec08e39d4c7b134227
Previous-Reviewed-on: https://chromium-review.googlesource.com/581431
(cherry picked from commit 53ffd29dc9cb5d2cfa8bdffd74cb903382e372d5)
Reviewed-on: https://chromium-review.googlesource.com/611208

lib/paygen/paygen_build_lib.py
@@ -65,9 +65,9 @@ RUN_SUITE_MIN_MSTONE = 30
 PAYGEN_LOG_TIMESTAMP_FORMAT = '%Y%m%d-%H%M%S-UTC'
 
 # Board and device information published by goldeneye.
-BOARDS_URI = 'gs://chromeos-build-release-console/boards.json'
 FSI_URI = 'gs://chromeos-build-release-console/fsis.json'
 OMAHA_URI = 'gs://chromeos-build-release-console/omaha_status.json'
+PAYGEN_URI = 'gs://chromeos-build-release-console/paygen.json'
 
 # Max number of attempts to download and parse a JSON file.
 JSON_PARSE_RETRY_COUNT = 2
@@ -397,8 +397,8 @@ class _PaygenBuild(object):
                          _FindControlFileDir(self._work_dir))
 
     # Cached goldeneye data.
-    self.cachedFsisJson = {}
-    self.cachedOmahaJson = {}
+    self.cachedFsisJson = None
+    self.cachedOmahaJson = None
 
   def _GetFsisJson(self):
     if not self.cachedFsisJson:
@@ -409,6 +409,55 @@ class _PaygenBuild(object):
     if not self.cachedOmahaJson:
       self.cachedOmahaJson = _GetJson(OMAHA_URI)
     return self.cachedOmahaJson
+
+  # Hidden class level cache value.
+  _cachedPaygenJson = None
+
+  @classmethod
+  def GetPaygenJson(cls, board=None, channel=None):
+    """Fetch the parsed Golden Eye payload generation configuration.
+
+    Args:
+      board: Board name in builder format (not release) or None for '*'
+      channel: Channel name in 'stable' or 'stable-channel' format.
+               Or None for any.
+
+    Returns:
+      List of GE delta values matching specification. Sample delta value:
+
+      {
+        'board': {
+          'public_codename': 'x86-alex-he',
+          'is_active': true,
+          'builder_name': 'x86-alex_he'
+        },
+        'delta_type': 'MILESTONE',
+        'channel': 'stable',
+        'chrome_os_version': '8530.81.0',
+        'chrome_version': '53.0.2785.103',
+        'milestone': 53,
+        'generate_delta': true,
+        'delta_payload_tests': true,
+        'full_payload_tests': false
+      }
+    """
+    # We express channels in two different namespaces. Convert to the
+    # namespace used by GE, if needed.
+    if channel and channel.endswith('-channel'):
+      channel = channel[:-len('-channel')]
+
+    if not cls._cachedPaygenJson:
+      cls._cachedPaygenJson = _GetJson(PAYGEN_URI)
+
+    result = []
+
+    for delta in cls._cachedPaygenJson['delta']:
+      if ((board and delta['board']['public_codename'] != board) or
+          (channel and delta['channel'] != channel)):
+        continue
+      result.append(delta)
+
+    return result
 
   def _GetFlagURI(self, flag):
     """Find the URI of the lock file associated with this build.
@@ -1398,17 +1447,13 @@ def ValidateBoardConfig(board):
   """Validate that we have config values for the specified |board|.
 
   Args:
-    board: Name of board to check.
+    board: Name of board to check in release namespace.
 
   Raises:
     BoardNotConfigured if the board is unknown.
   """
-  # Right now, we just validate that the board exists.
-  boards = _GetJson(BOARDS_URI)
-  for b in boards.get('boards', []):
-    if b['public_codename'] == board:
-      return
-  raise BoardNotConfigured(board)
+  if not _PaygenBuild.GetPaygenJson(board):
+    raise BoardNotConfigured(board)
 
 
 def CreatePayloads(build, work_dir, site_config,
@@ -1433,8 +1478,6 @@ def CreatePayloads(build, work_dir, site_config,
     run_parallel: Generate payloads in parallel processes.
     skip_duts_check: Do not force checking minimum available DUTs
   """
-  ValidateBoardConfig(build.board)
-
   return _PaygenBuild(build, work_dir, site_config,
                       dry_run=dry_run,
                       ignore_finished=ignore_finished,