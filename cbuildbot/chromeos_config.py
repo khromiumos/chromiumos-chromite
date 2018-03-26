f2d5d034cc0413709d1f13c72d43401a7d657bc5
Philip Chen <philipchen@google.com>
2018-03-26 16:49:02 -0700

    nautilus: Add factory builder
    BUG=b:67063006
    TEST=tryjob
    
    Change-Id: I950ac355a6933ee39998a59fae59deaea7754c40
    Reviewed-on: https://chromium-review.googlesource.com/980801
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Philip Chen <philipchen@chromium.org>
    Commit-Queue: Philip Chen <philipchen@chromium.org>
    Tested-by: Philip Chen <philipchen@chromium.org>

--- cbuildbot/chromeos_config.py
+++ cbuildbot/chromeos_config.py
@@ -3782,7 +3782,8 @@ def SpecialtyBuilders(site_config, boards_dict, ge_build_config):
   """
   board_configs = CreateInternalBoardConfigs(
       site_config, boards_dict, ge_build_config)
-  hw_test_list = HWTestList(ge_build_config)
+  # This is not used in a factory branch.
+  #hw_test_list = HWTestList(ge_build_config)
 
   site_config.AddWithoutTemplate(
       'success-build',
@@ -3935,15 +3936,14 @@ def SpecialtyBuilders(site_config, boards_dict, ge_build_config):
 
   # *-pre-flight-branch builders are in chromeos_release waterfall.
   site_config.Add(
-      'samus-chrome-pre-flight-branch',
+      'nautilus-pre-flight-branch',
       site_config.templates.pre_flight_branch,
-      display_label=config_lib.DISPLAY_LABEL_CHROME_PFQ,
-      boards=['samus'],
-      afdo_generate=True,
-      afdo_update_ebuild=True,
-      sync_chrome=True,
+      site_config.templates.no_vmtest_builder,
+      site_config.templates.no_hwtest_builder,
+      display_label=config_lib.DISPLAY_LABEL_FACTORY,
+      boards=['nautilus'],
+      sync_chrome=False,
       chrome_rev=constants.CHROME_REV_STICKY,
-      hw_tests=[hw_test_list.AFDORecordTest()],
       useflags=append_useflags(['-transparent_hugepage',
                                 '-debug_fission',
                                 '-thinlto']),
