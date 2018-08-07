a2174c550a8800c91c7c4bf18f70307c332ce32e
Gregory Meinke <gmeinke@google.com>
2018-08-07 16:43:11 -0600

    default GetLuciAuth to latest SHA1 version
    BUG=chromium:871831
    TEST=local unit test
    
    default to latest SHA1 instead of latest to
    fix old branches.
    
    Change-Id: Ib14ec566181908a054c14f2d84b48ff5af0e76e6
    (cherry picked from commit dd861a00fcd4134f9eecddf9607dd2af067113aa)
    Reviewed-on: https://chromium-review.googlesource.com/1168252
    Reviewed-by: Gregory Meinke <gmeinke@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Gregory Meinke <gmeinke@chromium.org>

--- lib/auth.py
+++ lib/auth.py
@@ -19,14 +19,18 @@ from chromite.lib import path_util
 REFRESH_STATUS_CODES = [401]
 
 
-def _GetAuthUtil(instance_id='latest'):
+# crbug:871831 default to last sha1 version.
+def _GetAuthUtil(
+    instance_id='git_revision:fd059ace316e4dbcaa5afdcec9ed4a855c4f3c65'):
   """Returns a path to the authutil binary.
 
   This will download and install the authutil package if it is not already
   deployed.
 
   Args:
-    instance_id: The instance-id of the package to install. Defaults to 'latest'
+    instance_id: The instance-id of the package to install. Defaults to
+    'git_revision:fd059ace316e4dbcaa5afdcec9ed4a855c4f3c65' which is the last
+    SHA1 version.
 
   Returns:
     the path to the authutil binary.
