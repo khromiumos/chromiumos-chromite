8c73b6c3cd7a65b0391c7b382312c3203c10feb7
YH Lin <yueherngl@google.com>
2018-10-16 12:26:39 -0700

    grunt: adding factory builder
    BUG=b:79997029
    TEST=cros tryjob -g '1283893' --branch=factory-grunt-11164.B grunt-factory-tryjob
    
    Change-Id: Id40f2d71270ad7aed10a4270044150aa8dd75609
    Reviewed-on: https://chromium-review.googlesource.com/c/1283893
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Commit-Queue: YH Lin <yueherngl@chromium.org>
    Tested-by: YH Lin <yueherngl@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -4115,7 +4115,8 @@ def SpecialtyBuilders(site_config, boards_dict, ge_build_config):
   """
   board_configs = CreateInternalBoardConfigs(
       site_config, boards_dict, ge_build_config)
-  hw_test_list = HWTestList(ge_build_config)
+  # This is not being used in this branch
+  # hw_test_list = HWTestList(ge_build_config)
 
   site_config.AddWithoutTemplate(
       'success-build',
@@ -4279,21 +4280,21 @@ def SpecialtyBuilders(site_config, boards_dict, ge_build_config):
 
   # *-pre-flight-branch builders are in chromeos_release waterfall.
   site_config.Add(
-      'samus-chrome-pre-flight-branch',
+      'grunt-pre-flight-branch',
       site_config.templates.pre_flight_branch,
-      display_label=config_lib.DISPLAY_LABEL_CHROME_PFQ,
-      boards=['samus'],
-      afdo_generate=True,
-      afdo_update_ebuild=True,
+      site_config.templates.no_vmtest_builder,
+      site_config.templates.no_hwtest_builder,
+      display_label=config_lib.DISPLAY_LABEL_FACTORY,
+      boards=['grunt'],
       sync_chrome=True,
       chrome_rev=constants.CHROME_REV_STICKY,
-      hw_tests=[hw_test_list.AFDORecordTest()],
       useflags=append_useflags(['-transparent_hugepage',
                                 '-debug_fission',
                                 '-thinlto',
                                 '-cfi']),
       prebuilts=constants.PRIVATE,
       archive_build_debug=True,
+      active_waterfall=waterfall.WATERFALL_BRANCH,
   )
 
   site_config.Add(
