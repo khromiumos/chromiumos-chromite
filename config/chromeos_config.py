62695d9565a7c68ffcc3817caf06c5fc9cd297b1
Zhuohao Lee <zhuohao@chromium.org>
2018-12-19 21:30:40 +0800

    rammus: add factory pre-flight builder
    BUG=b:111673473
    TEST=none
    
    Change-Id: Ia87a050cdffe029ed2955c4bb8b60fbf1c5165de
    Reviewed-on: https://chromium-review.googlesource.com/c/1384445
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Commit-Queue: Zhuohao Lee <zhuohao@chromium.org>
    Tested-by: Zhuohao Lee <zhuohao@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -3389,7 +3389,8 @@ def SpecialtyBuilders(site_config, boards_dict, ge_build_config):
   """
   board_configs = CreateInternalBoardConfigs(
       site_config, boards_dict, ge_build_config)
-  hw_test_list = HWTestList(ge_build_config)
+  # Remove this test from factory branch
+  # hw_test_list = HWTestList(ge_build_config)
 
   site_config.AddWithoutTemplate(
       'success-build',
@@ -3553,15 +3554,14 @@ def SpecialtyBuilders(site_config, boards_dict, ge_build_config):
 
   # *-pre-flight-branch builders are in chromeos_release waterfall.
   site_config.Add(
-      'samus-chrome-pre-flight-branch',
+      'rammus-pre-flight-branch',
       site_config.templates.pre_flight_branch,
-      display_label=config_lib.DISPLAY_LABEL_CHROME_PFQ,
-      boards=['samus'],
-      afdo_generate=True,
-      afdo_update_ebuild=True,
-      sync_chrome=True,
+      site_config.templates.no_vmtest_builder,
+      site_config.templates.no_hwtest_builder,
+      display_label=config_lib.DISPLAY_LABEL_FACTORY,
+      boards=['rammus'],
+      sync_chrome=False,
       chrome_rev=constants.CHROME_REV_STICKY,
-      hw_tests=[hw_test_list.AFDORecordTest()],
       useflags=config_lib.append_useflags(['-transparent_hugepage',
                                            '-debug_fission',
                                            '-thinlto',
