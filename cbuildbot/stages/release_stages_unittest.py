5fdcd3a558212b388fda7cad68ee4975f6a21cad
Xixuan Wu <xixuan@google.com>
2019-01-28 20:12:39 -0800

    PaygenTest: Refactor to retrieve each payload test config.
    This CL refactors paygen_build_lib to retrieve each payload test config
    for later scheduling with skylab tool.
    
    BUG=chromium:920393
    TEST=Ran unittest.
    
    Change-Id: I35248fa0994ae91b0ab382c449ae0de0e3aca371
    Reviewed-on: https://chromium-review.googlesource.com/1441915
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Xixuan Wu <xixuan@chromium.org>
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    (cherry picked from commit 4d94387e6ea7aade25f278f370d9d70e9346a816)
    Reviewed-on: https://chromium-review.googlesource.com/c/1484371
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Commit-Queue: Xixuan Wu <xixuan@chromium.org>

--- cbuildbot/stages/release_stages_unittest.py
+++ cbuildbot/stages/release_stages_unittest.py
@@ -316,6 +316,7 @@ class PaygenStageTest(generic_stages_unittest.AbstractStageTestCase,
         'foo-suite-name',
         'foo-archive-board',
         'foo-archive-build',
+        [],
     )])
 
   # pylint: disable=arguments-differ
